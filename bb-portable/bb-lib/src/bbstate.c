56405f76742b97a9cc0fb43f80c3c2cd5a74c80b
sacca97 <tommaso.sacchetti@gmail.com>
2025-06-12 16:32:10 +0200

    more bb-pairing

--- bb-portable/bb-lib/src/bbstate.c
+++ bb-portable/bb-lib/src/bbstate.c
@@ -3,17 +3,18 @@
 #include "crypto/blake2.h"
 #include <stdio.h>
 
-void print_buf(void *buf, size_t buf_len)
+void
+print_buf(void* buf, size_t buf_len)
 {
-    uint8_t *bufr = (uint8_t *)buf;
-    for (int i = 0; i < buf_len; i++)
-    {
+    uint8_t* bufr = (uint8_t*)buf;
+    for (int i = 0; i < buf_len; i++) {
         printf("%02x", bufr[i]);
     }
     printf("\n");
 }
 
-void mixhash(bbstate *st, const uint8_t *src, size_t src_len)
+void
+mixhash(bbstate* st, const uint8_t* src, size_t src_len)
 {
     blake2b_state ctx;
     blake2b_init(&ctx, HASH_LEN);
@@ -22,8 +23,9 @@ void mixhash(bbstate *st, const uint8_t *src, size_t src_len)
     blake2b_final(&ctx, st->hc, HASH_LEN);
 }
 
-void bbstate_init(bbstate *st, int role, uint8_t *s_pub, uint8_t *s_priv,
-                  uint8_t *rs, uint8_t *psk)
+void
+bbstate_init(bbstate* st, int role, uint8_t* s_pub, uint8_t* s_priv,
+             uint8_t* rs, uint8_t* psk)
 {
     st->role = role;
     st->counter = 0;
@@ -31,23 +33,19 @@ void bbstate_init(bbstate *st, int role, uint8_t *s_pub, uint8_t *s_priv,
     // I should zero the whole state
     memset(st->hc, 0, sizeof(st->hc));
 
-    if (s_pub && s_priv)
-    {
+    if (s_pub && s_priv) {
         memcpy(st->static_public_key, s_pub, KEY_LEN);
         memcpy(st->static_private_key, s_priv, KEY_LEN);
     }
 
     // BB-session assumes both possess remote static keys
-    if (rs)
-    {
+    if (rs) {
         memcpy(st->rs, rs, KEY_LEN);
-        if (role == 0)
-        {
+        if (role == 0) {
             mixhash(st, st->rs, KEY_LEN);
             mixhash(st, st->static_public_key, KEY_LEN);
         }
-        else
-        {
+        else {
             mixhash(st, st->static_public_key, KEY_LEN);
             mixhash(st, st->rs, KEY_LEN);
         }
@@ -61,9 +59,10 @@ void bbstate_init(bbstate *st, int role, uint8_t *s_pub, uint8_t *s_priv,
     mixhash(st, init_hash, sizeof(init_hash));
 }
 
-void bb_pairing_req_build(bbstate *st, uint8_t *msgbuf)
+void
+bb_pairing_req_build(bbstate* st, uint8_t* msgbuf)
 {
-    struct bb_session_start_req *msg = (struct bb_session_start_req *)msgbuf;
+    struct bb_session_start_req* msg = (struct bb_session_start_req*)msgbuf;
     memset(msg->reserved, 0, sizeof(msg->reserved));
     msg->counter = st->counter;
 
@@ -73,7 +72,8 @@ void bb_pairing_req_build(bbstate *st, uint8_t *msgbuf)
     memset(msg->auth, 0, sizeof(msg->auth));
 }
 
-void bb_session_start_build(bbstate *st, struct bb_session_start_req *msg)
+void
+bb_session_start_build(bbstate* st, struct bb_session_start_req* msg)
 {
     memset(msg->reserved, 0, sizeof(msg->reserved));
     msg->counter = st->counter;
@@ -91,24 +91,27 @@ void bb_session_start_build(bbstate *st, struct bb_session_start_req *msg)
     mixhash(st, &msg->type, sizeof(*msg));
 }
 
-void bb_session_start_req(bbstate *st, uint8_t *msgbuf)
+void
+bb_session_start_req(bbstate* st, uint8_t* msgbuf)
 {
-    struct bb_session_start_req *msg = (struct bb_session_start_req *)msgbuf;
+    struct bb_session_start_req* msg = (struct bb_session_start_req*)msgbuf;
     msg->type = BB_SESSION_START_REQ;
     bb_session_start_build(st, msg);
 }
 
-void bb_session_start_rsp(bbstate *st, uint8_t *msgbuf)
+void
+bb_session_start_rsp(bbstate* st, uint8_t* msgbuf)
 {
-    struct bb_session_start_req *msg = (struct bb_session_start_req *)msgbuf;
+    struct bb_session_start_req* msg = (struct bb_session_start_req*)msgbuf;
     msg->type = BB_SESSION_START_RSP;
     bb_session_start_build(st, msg);
 }
 
 // Processing is symmetric
-void bb_session_start_rx(bbstate *st, uint8_t *msgbuf)
+void
+bb_session_start_rx(bbstate* st, uint8_t* msgbuf)
 {
-    struct bb_session_start_req *msg = (struct bb_session_start_req *)msgbuf;
+    struct bb_session_start_req* msg = (struct bb_session_start_req*)msgbuf;
 
     memcpy(st->re, msg->ephemeral, KEY_LEN);
     mixhash(st, st->re, KEY_LEN);
@@ -124,17 +127,17 @@ void bb_session_start_rx(bbstate *st, uint8_t *msgbuf)
     // TOD): if decrypt fails means auth failed here we abort
 
     // else do this
-    if (msg->counter < st->counter)
-    {
+    if (msg->counter < st->counter) {
         // Counter is too old, ignore the message
         return;
     }
     // Otherwise counter was already updated in the decrypt function
 }
 
-void bb_pair_req_build(bbstate *st, uint8_t *msgbuf)
+void
+bb_pair_req_build(bbstate* st, uint8_t* msgbuf)
 {
-    struct bb_pair_req *msg = (struct bb_pair_req *)msgbuf;
+    struct bb_pair_req* msg = (struct bb_pair_req*)msgbuf;
     memset(msg->reserved, 0, sizeof(msg->reserved));
     msg->type = BB_PAIR_REQ;
     msg->iocap = 0; // TODO: Set IO capabilities
@@ -150,12 +153,12 @@ void bb_pair_req_build(bbstate *st, uint8_t *msgbuf)
 }
 
 // also sends the rsp
-void bb_pair_req_rx(bbstate *st, uint8_t *msgbuf)
+void
+bb_pair_req_rx(bbstate* st, uint8_t* msgbuf)
 {
-    struct bb_pair_req *msg = (struct bb_pair_req *)msgbuf;
+    struct bb_pair_req* msg = (struct bb_pair_req*)msgbuf;
 
-    if (msg->counter < st->counter)
-    {
+    if (msg->counter < st->counter) {
         // Counter is too old, ignore the message
         return;
     }
@@ -174,7 +177,6 @@ void bb_pair_req_rx(bbstate *st, uint8_t *msgbuf)
     ecdh_make_shared(st->key, msg->ephemeral, st->ephmeral_private_key);
 
     hkdf(st->key, NULL, NULL, st->key, st->hc, sizeof(st->hc));
-    print_buf(st->key, sizeof(st->key));
 
     memcpy(msg->ephemeral, st->ephmeral_public_key, KEY_LEN);
     mixhash(st, st->ephmeral_public_key, KEY_LEN);
@@ -183,15 +185,14 @@ void bb_pair_req_rx(bbstate *st, uint8_t *msgbuf)
                  0);
 
     mixhash(st, &msg->type, sizeof(*msg));
-    print_buf(st->hc, sizeof(st->hc));
 }
 
-void bb_pair_rsp_rx(bbstate *st, uint8_t *msgbuf)
+void
+bb_pair_rsp_rx(bbstate* st, uint8_t* msgbuf)
 {
-    struct bb_pair_req *msg = (struct bb_pair_req *)msgbuf;
+    struct bb_pair_req* msg = (struct bb_pair_req*)msgbuf;
 
-    if (msg->counter < st->counter)
-    {
+    if (msg->counter < st->counter) {
         // Counter is too old, ignore the message
         return;
     }
@@ -205,6 +206,30 @@ void bb_pair_rsp_rx(bbstate *st, uint8_t *msgbuf)
 
     aead_decrypt(NULL, st->key, msg->counter, st->hc, sizeof(st->hc), msg->auth,
                  sizeof(msg->auth));
+
+    mixhash(st, &msg->type, sizeof(*msg));
+}
+
+void
+bb_pair_pubkey_build(bbstate* st, uint8_t* msgbuf)
+{
+    struct bb_pair_pubkey* msg = (struct bb_pair_pubkey*)msgbuf;
+    msg->counter = st->counter;
+    aead_encrypt(msg->key, st->key, st->counter, st->hc, 32,
+                 st->static_public_key, KEY_LEN);
+    print_buf(st->static_public_key, KEY_LEN);
+    mixhash(st, &msg->type, sizeof(*msg));
+}
+
+void
+bb_pair_pubkey_rx(bbstate* st, uint8_t* msgbuf)
+{
+    struct bb_pair_pubkey* msg = (struct bb_pair_pubkey*)msgbuf;
+
+    aead_decrypt(st->rs, st->key, msg->counter, st->hc, 32, msg->key,
+                 KEY_LEN + TAG_LEN);
+
+    print_buf(st->rs, KEY_LEN);
 
     mixhash(st, &msg->type, sizeof(*msg));
 }
--- bb-portable/tests.c
+++ bb-portable/tests.c
@@ -13,17 +13,18 @@ static uint8_t priv_c[32];
 static uint8_t pub_p[32];
 static uint8_t priv_p[32];
 
-void print_buf(void *buf, size_t buf_len)
+void
+print_buf(void* buf, size_t buf_len)
 {
-    uint8_t *bufr = (uint8_t *)buf;
-    for (int i = 0; i < buf_len; i++)
-    {
+    uint8_t* bufr = (uint8_t*)buf;
+    for (int i = 0; i < buf_len; i++) {
         printf("%02x", bufr[i]);
     }
     printf("\n");
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char** argv)
 {
 
     uint8_t buffer[128];
@@ -31,18 +32,14 @@ int main(int argc, char **argv)
     uint8_t shared_c[32];
     uint8_t shared_p[32];
 
-    // BB-session
-
-    for (int i = 0; i < 10; i++)
-    {
+    /* BB-session */
+    for (int i = 0; i < 10; i++) {
         ecdh_keygen(pub_c, priv_c);
         ecdh_keygen(pub_p, priv_p);
 
         bbstate_init(&central, BB_ROLE_CENTRAL, pub_c, priv_c, pub_p, NULL);
         bbstate_init(&peripheral, BB_ROLE_PERIPHERAL, pub_p, priv_p, pub_c,
                      NULL);
-
-        // assert(memcmp(central.hc, peripheral.hc, sizeof(central.hc)) == 0);
 
         bb_session_start_req(&central, buffer);
 
@@ -62,19 +59,28 @@ int main(int argc, char **argv)
     ecdh_keygen(pub_p, priv_p);
 
     bbstate_init(&central, BB_ROLE_CENTRAL, pub_c, priv_c, NULL, NULL);
-    bbstate_init(&peripheral, BB_ROLE_PERIPHERAL, pub_p, priv_p, NULL,
-                 NULL);
+    bbstate_init(&peripheral, BB_ROLE_PERIPHERAL, pub_p, priv_p, NULL, NULL);
 
     // print_buf(central.hc, 32);
     // print_buf(peripheral.hc, 32);
 
     // assert(memcmp(central.hc, peripheral.hc, sizeof(central.hc)) == 0);
 
+    /* BB-pairing */
+
     bb_pair_req_build(&central, buffer);
 
     bb_pair_req_rx(&peripheral, buffer);
 
     bb_pair_rsp_rx(&central, buffer);
+
+    bb_pair_pubkey_build(&central, buffer);
+
+    bb_pair_pubkey_rx(&peripheral, buffer);
+
+    assert(memcmp(central.hc, peripheral.hc, sizeof(central.hc)) == 0);
+
+    assert(memcmp(central.static_public_key, peripheral.rs, KEY_LEN) == 0);
 
     // OK
 }
